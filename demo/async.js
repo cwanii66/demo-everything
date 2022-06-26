cdfd4c1c50933966693dcf4db69dfc5a0c3a4243
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-06-26 16:09:43 +0800

    async spawn

---
+++ demo/async.js
@@ -0,0 +1,33 @@
+async function fn(args) {
+    // ...
+}
+// equals to
+function fn_(args) {
+    return spawn(function* () {
+        // ...
+    });
+}
+
+// spawn => 自动执行器函数
+function spawn(genF) {
+    return new Promise(function(resolve, reject) {
+        const gen = genF();
+        function step(nextF) {
+            let next;
+            try {
+                next = nextF();
+            } catch(e) {
+                return reject(e);
+            }
+            if (next.done) {
+                return resolve(next.value);
+            }
+            Promise.resolve(next.value).then(function(v) {
+                step(function() { return gen.next(v); });
+            }, function(e) {
+                step(function() { return gen.throw(e); });
+            });
+        }
+        step(function() { return gen.next((void 0)); });
+    });
+}
